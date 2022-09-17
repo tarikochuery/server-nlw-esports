2dde97f16a975496b08173706af35c8bd256abeb
tariko <ttarikechuery@gmail.com>
2022-09-17 14:44:10 -0300

    Trying to set new config

--- build/utils/convertMinutesToHourString.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.convertMinutesToHourString = void 0;
-const convertMinutesToHourString = (minutesAmount) => {
-    const hour = Math.floor(minutesAmount / 60);
-    const minutes = (minutesAmount % 60);
-    return `${String(hour).padStart(2, '0')}:${String(minutes).padStart(2, '0')}`;
-};
-exports.convertMinutesToHourString = convertMinutesToHourString;
